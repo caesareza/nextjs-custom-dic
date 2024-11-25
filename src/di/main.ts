fc9f1fdc182c2a5ebc59451eb7820957642496fa
dreas <andreas.caesareza@eraspace.id>
2024-11-25 08:41:27 +0700

    - Refactor DIContainer with proper type - move all the message function to MessageService.ts

--- src/di/main.ts
+++ src/di/main.ts
@@ -1,24 +1,32 @@
-import { DIContainer } from '@/di/container';
-import { Services } from '@/services';
-import { envAdapter } from '@/services/envAdapter';
+import { DIContainer } from '@/di/container'
+import { Services } from '@/services'
+import { envAdapter } from '@/services/envAdapter'
 
 interface EnvAdapter {
-    APP_BASE_URL: string;
-    API_KEY: string;
-    NODE_ENV: string;
+    APP_BASE_URL: string
+    API_KEY: string
+    NODE_ENV: string
 }
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 type ServiceMap = {
-    envAdapter: EnvAdapter;
-} & Record<string, { execute: (...args: any[]) => any }>;
+    envAdapter: EnvAdapter
+} & Record<string, { execute: (...args: any[]) => any }>
 
-const di = new DIContainer<ServiceMap>();
+const di = new DIContainer<ServiceMap>()
 
+/*
+    Object.entries(Services)
+    this will register all the function through forEach loop
+ */
 Object.entries(Services).forEach(([key, service]) => {
-    di.register(key, service());
-});
+    di.register(key, service())
+})
 
-di.register('envAdapter', envAdapter);
+/*
+    this line here do a register static variable
+    (e.g. ENV, Constant, route)
+ */
+di.register('envAdapter', envAdapter)
 
-export default di;
+export default di
